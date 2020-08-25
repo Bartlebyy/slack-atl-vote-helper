254d338cfdc15e59b66b2a748e54e73320349e00
Glitch (hello-express) <none>
2020-08-25 23:11:35 +0000

    🤑🦁 Checkpoint ./app.js:21551179/7281

--- app.js
+++ app.js
@@ -68,4 +68,40 @@ const app = new App({
       console.error(error);
     }
   });
+
+  app.command("/helloworld", async ({ ack, payload, context }) => {
+    // Acknowledge the command request
+    ack();
+
+    try {
+      const result = await app.client.chat.postMessage({
+        token: context.botToken,
+        // Channel to send message to
+        channel: payload.channel_id,
+        // Include a button in the message (or whatever blocks you want!)
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "Go ahead. Click it."
+            },
+            accessory: {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Click me!"
+              },
+              action_id: "button_abc"
+            }
+          }
+        ],
+        // Text in the notification
+        text: "Message from Test App"
+      });
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 })();
